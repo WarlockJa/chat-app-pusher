71c57ef74ed7e596320319a534d56c5c63452f7d
warlockja <warlockja@gmail.com>
2024-03-14 14:04:49 +0300

    TS cleanup

--- src/app/components/Chat/types.d.ts
+++ src/app/components/Chat/types.d.ts
@@ -1,33 +1,15 @@
-interface IMessageData {
-  id: string;
-  message: string;
-  author: string;
-}
+// type for anonymous user data stored in localStorage
 interface IUserLocalStorageData {
   user_id: string;
   user_name: string;
   storage_uuid: string;
 }
 
+// initial user ID type passed to Chat element
 interface IInitUserId {
   user_id?: string;
   user_name?: string;
   user_admin?: boolean;
-}
-
-interface IChatProps extends IInitUserId {
-  storage_uuid?: string;
-  pageLimit?: number;
-}
-
-interface IUserInfo {
-  user_admin: boolean;
-  user_name: string;
-}
-
-interface ITriggerEventData {
-  id: string;
-  info: IUserInfo;
 }
 
 // pusher.channel("channel-name").members.members
@@ -41,8 +23,10 @@ interface IChannelMembers {
 // Object.values(pusher.channel("channel-name").members.members.get("user_name"))
 type IChannelGetMember = [string, { user_admin: boolean; user_name: string }];
 
+// type to track async state
 type TStateLiteral = "loading" | "success" | "error";
 
+// type for typing Pusher event timeout
 interface ITypingUserTimeout {
   id: string;
   timeout: NodeJS.Timeout;
@@ -54,6 +38,7 @@ interface IIODebouncedEntry {
   entryTimestamp: number;
 }
 
+// additional optional parameters passed to Chat element
 interface IChatProps extends IInitUserId {
   storage_uuid: string;
   pageLimit?: number;
